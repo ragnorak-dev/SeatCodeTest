52d1dc1ddacec46553fdaf7e7f5d82e91e373460
Ragnorak-dev <raulillangarcia@gmail.com>
2025-03-27 18:09:17 +0100

    little view changes

--- output/src/main/java/com/ragnorak/seatcodetest/output/ui/RoverCoorReceivedScreen.kt
+++ output/src/main/java/com/ragnorak/seatcodetest/output/ui/RoverCoorReceivedScreen.kt
@@ -1,13 +1,16 @@
 package com.ragnorak.seatcodetest.output.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -24,19 +27,27 @@ fun RoverCoorReceiverScreen(viewModel: RoverCoorReceiverViewModel = hiltViewMode
         }
         is RoverCoorReceiverState.Success -> {
 
-             Box(modifier = Modifier.fillMaxSize()) {
+             Row(modifier = Modifier
+                 .padding(dimensionResource(R.dimen.screen_padding))
+                 .fillMaxSize()) {
+
+                 Text(modifier = Modifier.weight(2F),
+                     text = "${stringResource(R.string.current_position)}: ${value.data.last().movement}",
+                     style = MaterialTheme.typography.titleLarge
+                 )
+
                  LazyColumn(
-                     modifier = Modifier.align(Alignment.TopEnd)
+                     modifier = Modifier.weight(1F),
+                     horizontalAlignment = Alignment.Start
                  ) {
+                     item {
+                         Text(text = stringResource(R.string.movement_history))
+                     }
                      items(value.data.size, key = { it }) {
                          Text(text = value.data[it].movement)
                      }
                  }
-                 Text(
-                     modifier = Modifier.align(Alignment.Center),
-                     text = "${stringResource(R.string.current_position)}: ${value.data.last().movement}",
-                     style = MaterialTheme.typography.titleLarge
-                 )
+
              }
 
         }
